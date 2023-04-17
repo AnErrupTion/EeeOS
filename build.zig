c67c173d849ba86276746722d3e101c472379c66
AnErrupTion <anerruption@disroot.org>
2023-04-17 18:05:36 +0200

    Add make-iso and run build steps

--- build.zig
+++ build.zig
@@ -3,12 +3,7 @@ const std = @import("std");
 const arch = enum { unspecified, i386 };
 
 pub fn build(b: *std.build.Builder) void {
-    std.fs.cwd().makeDir("iso") catch {};
-
-    var isoDirectory = std.fs.cwd().openDir("iso", std.fs.Dir.OpenDirOptions{}) catch unreachable;
-    defer isoDirectory.close();
-
-    var build_arch = b.option(arch, "arch", "Build for the i386 architecture") orelse arch.unspecified;
+    var build_arch = b.option(arch, "arch", "Build for a specified architecture") orelse arch.unspecified;
 
     const exe = b.addExecutable("EeeOS", "src/main.zig");
     exe.code_model = std.builtin.CodeModel.kernel;
@@ -37,23 +32,13 @@ pub fn build(b: *std.build.Builder) void {
 
     exe.install();
 
-    //std.fs.cwd().copyFile("limine/limine-cd.bin", isoDirectory, "limine-cd.bin", std.fs.CopyFileOptions{}) catch unreachable;
-    //std.fs.cwd().copyFile("limine/limine.sys", isoDirectory, "limine.sys", std.fs.CopyFileOptions{}) catch unreachable;
-    //std.fs.cwd().copyFile("limine/limine.cfg", isoDirectory, "limine.cfg", std.fs.CopyFileOptions{}) catch unreachable;
-    //std.fs.cwd().copyFile("zig-out/bin/EeeOS", isoDirectory, "EeeOS.elf", std.fs.CopyFileOptions{}) catch unreachable;
+    const make_iso_step = b.step("make-iso", "Create a bootable ISO image");
+    make_iso_step.makeFn = make_iso;
+    make_iso_step.dependOn(b.getInstallStep());
 
-    //const args: [*:null]const ?[*:0]const u8 = ;
-
-    //std.os.execvpeZ("xorriso", args, null);
-
-    //const run_cmd = exe.run();
-    //run_cmd.step.dependOn(b.getInstallStep());
-    //if (b.args) |args| {
-    //    run_cmd.addArgs(args);
-    //}
-
-    //const run_step = b.step("run", "Run the app");
-    //run_step.dependOn(&run_cmd.step);
+    const run_step = b.step("run", "Runs the operating system");
+    run_step.makeFn = run;
+    run_step.dependOn(make_iso_step);
 
     //const exe_tests = b.addTest("src/main.zig");
     //exe_tests.setTarget(target);
@@ -61,4 +46,61 @@ pub fn build(b: *std.build.Builder) void {
 
     //const test_step = b.step("test", "Run unit tests");
     //test_step.dependOn(&exe_tests.step);
+}
+
+fn make_iso(self: *std.build.Step) !void {
+    _ = self;
+
+    var current_dir = std.fs.cwd();
+
+    current_dir.makeDir("iso") catch {};
+
+    var isoDirectory = current_dir.openDir("iso", std.fs.Dir.OpenDirOptions{}) catch unreachable;
+    defer isoDirectory.close();
+
+    current_dir.copyFile("limine/limine-cd.bin", isoDirectory, "limine-cd.bin", std.fs.CopyFileOptions{}) catch unreachable;
+    current_dir.copyFile("limine/limine.sys", isoDirectory, "limine.sys", std.fs.CopyFileOptions{}) catch unreachable;
+    current_dir.copyFile("limine/limine.cfg", isoDirectory, "limine.cfg", std.fs.CopyFileOptions{}) catch unreachable;
+    current_dir.copyFile("zig-out/bin/EeeOS", isoDirectory, "EeeOS.elf", std.fs.CopyFileOptions{}) catch unreachable;
+
+    const xorriso_argv = [_][]const u8{
+        "xorriso",
+        "-as",
+        "mkisofs",
+        "-b",
+        "limine-cd.bin",
+        "-no-emul-boot",
+        "-boot-load-size",
+        "4",
+        "-boot-info-table",
+        "iso",
+        "-o",
+        "EeeOS.iso",
+    };
+
+    _ = std.ChildProcess.exec(.{ .argv = &xorriso_argv, .allocator = std.heap.page_allocator }) catch unreachable;
+
+    const limine_deploy_argv = [_][]const u8{
+        "limine/limine-deploy",
+        "EeeOS.iso",
+    };
+
+    _ = std.ChildProcess.exec(.{ .argv = &limine_deploy_argv, .allocator = std.heap.page_allocator }) catch unreachable;
+}
+
+fn run(self: *std.build.Step) !void {
+    _ = self;
+
+    const qemu_argv = [_][]const u8{
+        "qemu-system-i386",
+        "-cpu",
+        "pentium2",
+        "-enable-kvm",
+        "-m",
+        "128M",
+        "-cdrom",
+        "EeeOS.iso",
+    };
+
+    _ = std.ChildProcess.exec(.{ .argv = &qemu_argv, .allocator = std.heap.page_allocator }) catch unreachable;
 }
